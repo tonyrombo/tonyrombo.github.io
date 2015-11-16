0247b581bbc6fdcb678a4f89ea3f037db2c9e3d4
tonyrombo <antony.romerobonilla@gmail.com>
2015-11-16 17:11:55 -0600

    se agrega pagina descriptiva para proyectos

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 	var windowHeight = $( window ).height(),
 		windowWidth = $(window).width();
 
-	$(function(){
-		$(".header").height(windowHeight);
-	});
+	if ($(".header")) {
+		$(function(){
+			$(".header").height(windowHeight);
+		});
+	};
 
 	if(windowWidth < 1000){
 		$("#show-menu, .navbar-item").click(function(){
