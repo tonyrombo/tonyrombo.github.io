80a3207159c78093d9c3dce8c626b33bd09e7d68
Antony Romero Bonilla <antony.romerobonilla@gmail.com>
2015-12-05 17:04:02 -0600

    se agregan paginas a proyectos faltantes y se elimina carpeta work

--- work/Auditorio/views/form-scripts.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo'  
-	<script type="text/javascript" src="js/buy_control.js"></script>
-	<script type="text/javascript" src="js/validation.js"></script>
-';
-?>
--- work/Auditorio/views/scripts.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo'
-	<script type="text/javascript" src="js/ajaxModule.js"></script>
-	<script type="text/javascript" src="js/main.js"></script>
-';
-?>
